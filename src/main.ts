810fac1b7414267b5504222c77062c0be2f1332f
egor-shishkarev <egorsiskarev92@gmail.com>
2024-05-11 15:33:58 +0300

    Try to fix argon2.hash problem

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { configDotenv } from 'dotenv';
+import * as argon2 from 'argon2';
 
 async function bootstrap() {
   configDotenv();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(argon2.hash("newadmin"));
 }
 bootstrap();
