691ea6650d9be43996aa96f26e198e617d84b0a0
egor-shishkarev <egorsiskarev92@gmail.com>
2024-05-11 16:00:04 +0300

    Add alert

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -42,9 +42,9 @@ export class AppController {
   async register(@Body() body: AccountDto) {
     const result = await this.databaseService.add(body);
     if (result) {
-      return "User successfully added";
+      alert("User successfully added");
     } else {
-      return "User with this login already exists";
+      alert("User with this login already exists");
     }
   }
 
